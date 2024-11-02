56c6c047c3fa1d3c269291dc70100136b64126d9
Prithivirajan2911 <prithivirajan2911@gmail.com>
2024-11-02 18:17:16 +0530

    ex6

--- Ex6.c
+++ Ex6.c
@@ -1,19 +1,3 @@
-# Ex-6-IMPLEMENTATION-OF-THE-BACK-END-OF-THE-COMPILER-
-IMPLEMENTATION OF THE BACK END OF THE COMPILER 
-## NAME: PRITHIVIRAJAN V  
-## REGISTERED NUMBER: 212223100042
-# Aim :
-To write a program to implement the back end of the compiler.
-# ALGORITHM
-1. Start the program.
-2. Get the three variables from statements and stored in the text file k.txt.
-3. Compile the program and give the path of the source file.
-4. Execute the program.
-5. Target code for the given statement is produced.
-6. Stop the program.
-# PROGRAM
-```
-
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
@@ -55,15 +39,3 @@ int main() {
     fclose(fp);
     return 0;
 }
-
-```
-# OUTPUT
-
-## k.txt File
-![txt-file](Ex6(k.txt).png)
-
-## Program Output
-![ex6](Ex6.png)
-
-# Result
-The back end of the compiler is implemented successfully, and the output is verified.
